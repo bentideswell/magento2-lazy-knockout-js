875c1402035eb3bd67dfb97a04fc2f21c27dd628
Ben Tideswell <ben@fishpig.co.uk>
2023-10-20 10:50:02 +0100

    1.0.5

--- view/frontend/web/js/messages/vanilla.js
+++ view/frontend/web/js/messages/vanilla.js
@@ -13,28 +13,24 @@ define([], function () {
     }
 
     return function (config, element) {
-        try {
-            var cookie = getCookie('mage-messages');
+        var cookie = getCookie('mage-messages');
 
-            if (!cookie) {
-                return false;
+        if (!cookie) {
+            return false;
+        }
+
+        var messages = JSON.parse(decodeURIComponent(getCookie('mage-messages')));
+        var wrapper = document.querySelector('div[data-placeholder="messages"]');
+        var template = document.getElementById('lazy-ko-message-template');
+
+        if (messages.length > 0) {
+            for (var i in messages) {
+                var text = messages[i]['text'], type = messages[i]['type'];
+                var html = template.innerHTML.replaceAll('<%= type %>', type).replaceAll('<%= text %>', text);
+                wrapper.innerHTML += html;
             }
 
-            var messages = JSON.parse(decodeURIComponent(getCookie('mage-messages')));
-            var wrapper = document.querySelector('div[data-placeholder="messages"]');
-            var template = document.getElementById('lazy-ko-message-template');
-
-            if (messages.length > 0) {
-                for (var i in messages) {
-                    var text = messages[i]['text'], type = messages[i]['type'];
-                    var html = template.innerHTML.replaceAll('<%= type %>', type).replaceAll('<%= text %>', text);
-                    wrapper.innerHTML += html;
-                }
-
-                deleteCookie('mage-messages');
-            }
-        } catch (e) {
-            window.alert(e);
+            deleteCookie('mage-messages');
         }
     }
 });
